e68a796ae667b9be095b426e932907c31cc4ef46
junhee-won <wwoon63@snu.ac.kr>
2023-01-02 22:50:47 +0900

    [feat] error 페이지 추가

---
+++ pages/_error.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default function ErrorPage() {
+  return <Container>error</Container>;
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
