8b89f28fcb0665ffd3b53d79a86fbc02a6775b28
junhee-won <wwoon63@snu.ac.kr>
2023-02-21 00:30:21 +0900

    [fix] build 에러 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
+        <script
+          src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"
+          async
+        ></script>
       </body>
     </Html>
   );
