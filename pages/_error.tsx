a555a25515413600928166a6f0f09a2d7ebf737e
junhee-won <wwoon63@snu.ac.kr>
2023-03-06 23:11:41 +0900

    [design] error 페이지

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -2,8 +2,11 @@ import styled from "styled-components";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../src/components/common/Header";
+import { useMedia } from "../src/hooks/useMedia";
 
 export default function ErrorPage() {
+  const media = useMedia();
   return (
     <Container>
       <Head>
@@ -12,9 +15,20 @@ export default function ErrorPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Text>페이지가 존재하지 않습니다</Text>
+      <Header media={media} />
+      <WarningWrapper>
+        <ConeWrapper isMobile={media === "mobile"}>
+          <Image src="/cone.svg" alt="cone icon" fill sizes="100%" priority />
+        </ConeWrapper>
+        <Text isMobile={media === "mobile"}>페이지가 존재하지 않습니다</Text>
+        <ConeWrapper isMobile={media === "mobile"}>
+          <Image src="/cone.svg" alt="cone icon" fill sizes="100%" priority />
+        </ConeWrapper>
+      </WarningWrapper>
       <Link href="/">
-        <Image src="/home.svg" alt="home page" width={200} height={200} />
+        <HomeWrapper isMobile={media === "mobile"}>
+          <Image src="/home.svg" fill sizes="100%" priority alt="home icon" />
+        </HomeWrapper>
       </Link>
     </Container>
   );
@@ -27,8 +41,30 @@ const Container = styled.div`
   align-items: center;
   gap: 100px;
   height: 100vh;
+  padding-top: 60px;
 `;
 
-const Text = styled.div`
-  font-size: 100px;
+const WarningWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const HomeWrapper = styled.div<{ isMobile: boolean }>`
+  position: relative;
+  width: ${(props) => (props.isMobile ? "50px" : "150px")};
+  height: ${(props) => (props.isMobile ? "50px" : "150px")};
+  object-fit: contain;
+`;
+
+const ConeWrapper = styled.div<{ isMobile: boolean }>`
+  position: relative;
+  width: ${(props) => (props.isMobile ? "30px" : "70px")};
+  height: ${(props) => (props.isMobile ? "30px" : "70px")};
+  object-fit: contain;
+`;
+
+const Text = styled.div<{ isMobile: boolean }>`
+  font-size: ${(props) => (props.isMobile ? "20px" : "70px")};
 `;
