f4029dbf05183d2ba5f48a8ee45142b6b7b71be8
junhee-won <wwoon63@snu.ac.kr>
2023-03-23 17:45:27 +0900

    fix: hljs script async -> sync, using next/script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
-          src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"
-          async
-        ></script>
+        <Script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></Script>
       </body>
     </Html>
   );
