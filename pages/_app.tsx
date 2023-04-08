ae2c66128c7bb22e6c2606d990c7fe70dfbc9495
junhee-won <wwoon63@snu.ac.kr>
2023-04-08 16:11:47 +0900

    feat: styled-components theme 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 import "../styles/stackoverflow-dark.min.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#D3563C",
+    secondary: "#E8E1D7",
+    danger: "#dc3545",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
