882ba9941d04f65c17d54af57fe82d81db595b57
junhee-won <wwoon63@snu.ac.kr>
2023-04-13 13:37:14 +0900

    feat: global style 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/stackoverflow-dark.min.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const theme = {
   colors: {
@@ -11,10 +12,19 @@ const theme = {
   },
 };
 
+const GlobalStyle = createGlobalStyle<{ backgroundColor: string }>`
+  body {
+    background-color: ${(props) => props.backgroundColor};
+  }
+`;
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <GlobalStyle backgroundColor={theme.colors.secondary} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
